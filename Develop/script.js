c9770f87034d42b9d80f742df8dee109617e105f
Athena <athenawilbon1994@aol.com>
2023-03-14 17:12:38 -0400

    Arrays added + PW function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
-// Assignment code here
-
+var passwordLength = 8;
+var choice = [];
+var specialCharacter = ['!','@','#','$','%','^','*','(',')','-','+'];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -13,5 +17,31 @@ function writePassword() {
 
 }
 
+function generatePassword () {
+  passwordLength = prompt("How many characters would you like for your password? Choose length between 8 and 128");
+  console.log("Length of password:" + passwordLength );
+
+}
+
+if(passwordLength < 8 || passwordLength > 128) { 
+  alert("Password length needs to be between 8 - 128 characters. ");
+  return false;
+}
+
+if (confirm("WIll your password contain Numbers?")){
+choice = choice.concat(numbers);
+}
+
+if (confirm("WIll your password contain Numbers?")){
+choice = choice.concat(numbers);
+}
+if (confirm("WIll your password contain Numbers?")){
+choice = choice.concat(numbers);
+}
+if (confirm("WIll your password contain Numbers?")){
+choice = choice.concat(numbers);
+}
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
