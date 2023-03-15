a1a4a4353695fa2abf48db2203d6d40268e3fff3
Athena <athenawilbon1994@aol.com>
2023-03-15 17:10:49 -0400

    Comments added to sections, console logg error added

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,5 @@
+// Assingment code
+// Variables in arrays to be called to the function
 var passwordLength = 8;
 var choice = [];
 var specialCharacter = ['!','@','#','$','%','^','*','(',')','-','+'];
@@ -5,43 +7,71 @@ var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
 var numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Event listener for a click to begin prompts
+
+generateBtn.addEventListener("click", writePassword);
+
+// Function "getPrompts" called, assigns to "passwordText' to generate" 
+// Error console logs error if untrue, then returns
 function writePassword() {
-  var password = generatePassword();
+  var confirmedPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
+  if (!confirmedPrompts) {
+    console.error("No prompts selected , please try again");
+    return;
+  }
+    var generatedPW = generatePassword(confirmedPrompts);
+    if(!generatedPW){
+      console.error("Password Failed to generate")
+    }
+    passwordText.value = generatedPW
+  }
+  
+// Password variable defined empty. For loop begins process of assigning to variable randomChoice * password length
 
 function generatePassword () {
-  passwordLength = prompt("How many characters would you like for your password? Choose length between 8 and 128");
-  console.log("Length of password:" + passwordLength );
+var password = "";
+for (var i = 0; i < passwordLength; i++) {
+    var randomChoice = Math.floor(Math.random() * choice.length);
+    password = password + choice[randomChoice];
+}
+return password;
+}
 
+//  Intializes prompts for user to choose password options
+
+function getPrompts() {
+  choice = [];
+
+// Password options begin and functions react in a true / false manner
+
+passwordLength = prompt("How many characters would you like for your password? Choose length between 8 and 128");
+
+if(!passwordLength){
+  alert("input Required");
 }
 
 if(passwordLength < 8 || passwordLength > 128) { 
-  alert("Password length needs to be between 8 - 128 characters. ");
-  return false;
+  passwordLength = alert("Password length needs to be between 8 - 128 characters. ");
+return false;
 }
 
-if (confirm("WIll your password contain Numbers?")){
+if (confirm("Will your password contain Numbers?")) {
 choice = choice.concat(numbers);
 }
 
-if (confirm("WIll your password contain Numbers?")){
-choice = choice.concat(numbers);
+if (confirm("Will your password contain Special Characters?")) {
+choice = choice.concat(specialCharacter);
 }
-if (confirm("WIll your password contain Numbers?")){
-choice = choice.concat(numbers);
+if (confirm("Will your password contain Lower Case Characters?")) {
+choice = choice.concat(lowerCase);
 }
-if (confirm("WIll your password contain Numbers?")){
-choice = choice.concat(numbers);
+if (confirm("WIll your password contain Uppercase Characters?")) {
+choice = choice.concat(upperCase);
 }
-}
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+return true; }
 
+// value returns true and loops back if user reclicks generate button
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Password Generator Starter Code
+# Password Generator
+
+## Description
+
+A password generating website designed for a useer to choose options for an automatically generated password. 
+
+![Alt text](Develop/Screenshot%202023-03-15%20170213.png)
+
+
+## Installation
+
+N/A
+
+## Usage
+
+For the user to have the option to choose length of their generated password while also deciding between uppercase, lowercase, special character and number options. Generates a randomized password based on the users choices. Password generation limited to a length between 8-128.
+
+
+## Credits
+
+https://developer.mozilla.org/en-US/
+https://stackoverflow.com/
+
+## License
+
+MIT License
+
+Copyright (c) 2023 AthenaBW
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
